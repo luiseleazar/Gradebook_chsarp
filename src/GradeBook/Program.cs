74c9a66d6b9b4e2acc048451eef4a1478a7d7d88
Luis Eleazar <eleanv05@gmail.com>
2022-03-22 21:29:21 -0700

    First commit, upload files with .gitignore's

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Eleazar's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
+            
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+
+        }
+    }
+}
