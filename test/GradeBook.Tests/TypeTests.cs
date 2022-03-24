532e2ca191324942dfa69238bda08acdd0f4d67c
Luis Eleazar <eleanv05@gmail.com>
2022-03-23 22:27:27 -0700

    Pass by value and pass by ref test

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -6,6 +6,55 @@ namespace GradeBook.Tests;
 public class TypeTests
 {
     [Fact]
+    public void CSharpCanPassByRef()
+    {
+        var book1 = GetBook("Book 1");
+        GetBookSetName(out book1, "New Name");
+
+        Assert.Same("New Name", book1.Name);
+    }
+
+    /// <summary>
+    /// Method <c>GetBookSetName</c> gets an object ref and assign a name
+    /// </summary>
+    private void GetBookSetName(out Book book, string name)
+    {
+        book = new Book(name);
+    }
+    [Fact]
+    public void CSharpIsPassedByValue()
+    {
+        var book1 = GetBook("Book 1");
+        GetBookSetName(book1, "New Name");
+
+        Assert.Same("Book 1", book1.Name);
+    }
+
+    /// <summary>
+    /// Method <c>GetBookSetName</c> constructus a new book instance and assign a name
+    /// </summary>
+    private void GetBookSetName(Book book, string name)
+    {
+        book = new Book(name);
+    }
+    [Fact]
+    public void CanSetNameFromReference()
+    {
+        var book1 = GetBook("Book 1");
+        SetName(book1, "New Name");
+
+        Assert.Same("New Name", book1.Name);
+    }
+
+    /// <summary>
+    /// Method <c>SetName</c> sets a new name to a book object
+    /// </summary>
+    private void SetName(Book book, string name)
+    {
+        book.Name = name;
+    }
+
+    [Fact]
     public void GetBookReturnsDifferentObjects()
     {
         var book1 = GetBook("Book 1");
