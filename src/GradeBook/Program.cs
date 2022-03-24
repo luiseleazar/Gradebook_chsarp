311b0ac8e3d92386cddd1c11bf40c2e9b9d80910
Luis Eleazar <eleanv05@gmail.com>
2022-03-23 21:38:25 -0700

    Build a sln and added its .gitignore file

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -16,7 +16,6 @@ namespace GradeBook
             Console.WriteLine($"The average grade is {stats.Average:N1}");
             Console.WriteLine($"The highest grade is {stats.High}");
             Console.WriteLine($"The lowest grade is {stats.Low}");
-
         }
     }
 }
